429b62b0858aac007b2e0df970025c1c3b6f61f7
BrunoDS01 <bdisanzo@itba.edu.ar>
2024-09-27 14:52:44 -0300

    Quiero agradecer a mi mamá
    Co-authored-by: Juan Sbruzzi <juan-sb@users.noreply.github.com>
    Co-authored-by: Ale Heir <alheir@users.noreply.github.com>
    Co-authored-by: CandeGioia <Cande-Gioia@users.noreply.github.com>

--- detection_helper.py
+++ detection_helper.py
@@ -2,7 +2,7 @@ import pandas as pd
 from PIL import Image
 import xml.etree.ElementTree as ET
 
-def create_DF(files,images_dir):
+def create_DF(files, images_dir):
     """
     Convierte una lista de PosixPaths en un dataframe
     """
@@ -32,16 +32,17 @@ def create_DF(files,images_dir):
         
         aux_df = pd.DataFrame(image_dic)
         
-        try:
-            image_file = next(images_dir.glob(f"**/{file.stem}*png"))
-            aux_df["height"] = root.find('size').find('height').text
-            aux_df["width"] = root.find('size').find('width').text
-            aux_df["image"] = image_file
-            aux_df["image_name"] = root.find('filename').text
-            df = df.append(aux_df, ignore_index=True)
-        except:
-            print(file.stem)
-            
+        # try:
+        image_file = next(images_dir.glob(f"**/{file.stem}*.png"))
+        aux_df["height"] = root.find('size').find('height').text
+        aux_df["width"] = root.find('size').find('width').text
+        aux_df["image"] = image_file
+        aux_df["image_name"] = root.find('filename').text
+        #df = df.append(aux_df, ignore_index=True)
+        pd.concat([df, aux_df])
+        # except:
+        #     print(file.stem)
+    print(df)
     df["center_x"] = (df["right"] + df["left"])/2
     df["center_y"] = (df["bottom"] + df["top"])/2
     df["delta_x"] = df["right"] - df["left"]
